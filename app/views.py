294ef1001d198c2a2ec3eaf8930779a08b2d89b2
Robert Elwell <robert@wikia-inc.com>
2013-12-10 17:00:19 -0800

    almost back to square one, just need to get routes sorted out. ugh!

--- app/views.py
+++ app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import models
+from django.shortcuts import render_to_response
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+
+def frontpage(request):
+    pass
+
+
+def subwikia(request, subdomain):
+    try:
+        wiki = models.Wiki.objects.get(subdomain=subdomain)
+    except ObjectDoesNotExist:
+        return four_oh_four(request)
+    stories = wiki.stories.get_queryset().order_by('-last_updated')
+    return render_to_response('subwikia.html', dict(wiki=wiki, stories=stories))
+
+
+def comments(request, subdomain, story_id):
+    pass
+
+
+def upvote(request, story_id):
+    pass
+
+
+def downvote(request, story_id):
+    pass
+
+
+def four_oh_four(request):
+    return render_to_response('404.html')
