9f82ff9a9c4556673df855ce6d6a8c60853f97d5
Robert Elwell <robert@wikia-inc.com>
2013-12-12 08:50:12 -0800

    working on heroku deployment

--- OurWikia/settings.py
+++ OurWikia/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
+import dj_database_url
 from registration_defaults.settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -114,3 +114,23 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ourwikiaapp@gmail.com'
 EMAIL_HOST_PASSWORD = 'our_wikia_app'
 EMAIL_USE_TLS = True;
+
+# Heroku ish
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
