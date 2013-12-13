32cc33d6c9aafae90d857eb1d3d6f18e508362f9
Robert Elwell <robert@wikia-inc.com>
2013-12-12 19:45:08 -0800

    views for up/down voting

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 import models
+import json
 from django.shortcuts import render_to_response
 from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
-from django.db.models import Max
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 
 def frontpage(request):
@@ -28,12 +31,56 @@ def comments(request, subdomain, story_id):
     pass
 
 
+"""
+It's a freaking hackathon, and these are vulnerable to CSRF, and I don't caaaaaaare
+"""
+
+@require_POST
+@login_required
 def upvote(request, story_id):
-    pass
+    try:
+        story = models.Story.get(id=story_id)
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps({'message': 'Story not found'}), status=404)
+    try:
+        current_downvote = request.user.downvotes.get(story_id=story_id)
+        current_downvote.delete()
+    except ObjectDoesNotExist:
+        pass
+    try:
+        request.user.upvotes.get(story_id=story_id)
+        return HttpResponse(json.dumps({'message': 'Already upvoted'}), status=200)
+    except ObjectDoesNotExist:
+        new_upvote = models.UpVote()
+        new_upvote.story = story
+        new_upvote.user = request.user
+        new_upvote.save()
+    return HttpResponse(json.dumps({'message': 'Upvote successful'}), status=200)
 
 
+
+
+@require_POST
+@login_required
 def downvote(request, story_id):
-    pass
+    try:
+        story = models.Story.get(id=story_id)
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps({'message': 'Story not found'}), status=404)
+    try:
+        current_upvote = request.user.upvotes.get(story_id=story_id)
+        current_upvote.delete()
+    except ObjectDoesNotExist:
+        pass
+    try:
+        request.user.downvotes.get(story_id=story_id)
+        return HttpResponse(json.dumps({'message': 'Already Downvoted'}), status=200)
+    except ObjectDoesNotExist:
+        new_downvote = models.DownVote()
+        new_downvote.story = story
+        new_downvote.user = request.user
+        new_downvote.save()
+    return HttpResponse(json.dumps({'message': 'Downvote successful'}), status=200)
 
 
 def four_oh_four(request):
