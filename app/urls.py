22f7d3fa7158c78b424b7f699f14d35d5f7e204f
Robert Elwell <robert@wikia-inc.com>
2013-12-12 19:45:08 -0800

    views for up/down voting

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ import views
 urlpatterns = patterns('',
                        url(r'w/(\w+)/?', views.subwikia),
                        url(r'w/([^/]+)/(\d+)/?', views.comments),
+                       url(r'w/(\d+)/upvote', views.upvote),
+                       url(r'w/(\d+)/downvote', views.downvote),
                        url(r'', views.frontpage))
