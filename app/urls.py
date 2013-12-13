8a89742e4002656c8528c7b06a753dd202f17bbd
Robert Elwell <robert@wikia-inc.com>
2013-12-13 12:09:54 -0800

    redirect profile

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
                        url(r'w/(\w+)/?', views.subwikia),
                        url(r'w/([^/]+)/(\d+)/?', views.comments),
                        url(r'(\d+)/upvote/?', views.upvote),
                        url(r'(\d+)/downvote/?', views.downvote),
+                       url(r'^accounts/profile/?$', RedirectView.as_view(url='/', permanent=False))
                        url(r'', views.frontpage))
