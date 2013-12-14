35f190944bcf47df5fe1a7f03181cf11a11f001f
Robert Elwell <robert@wikia-inc.com>
2013-12-13 16:44:46 -0800

    uncommented prod

--- OurWikia/settings.py
+++ OurWikia/settings.py
@@ -120,25 +120,20 @@ EMAIL_USE_TLS = True;
 # Heroku ish
 # Parse database configuration from $DATABASE_URL
 
-"""
-commented out for development because configuring a dev environment is a pain in the ass
+DATABASES['default'] = dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    DATABASES['default'] = dj_database_url.config()
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
-
-    # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-"""
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
